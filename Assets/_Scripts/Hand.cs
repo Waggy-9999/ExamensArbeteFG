cde045201bf07f169c0bf2a52dd610646d84d1d4
Viktor Palmén <viktor.palmen23@gmail.com>
2023-12-13 19:24:45 +0100

    Hand Easing
    Hand Script now eases cards

--- Assets/_Scripts/Hand.cs
+++ Assets/_Scripts/Hand.cs
@@ -1,34 +1,63 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Utility.Easing;
 
 public class Hand : MonoBehaviour
 {
     [SerializeField] private List<GameObject> cardsInHand = new List<GameObject>(); // List to store the cards in hand
     [SerializeField] private GameObject cardPrefab; // Prefab for the card
+    [SerializeField] private GameObject spawnPoint;
 
     // Method to add a card to the hand
     public void AddCard()
     {
-        GameObject newCard = Instantiate(cardPrefab, transform.position, Quaternion.identity); // Instantiate a new card from the prefab
+        GameObject newCard = Instantiate(cardPrefab, spawnPoint.transform.position, Quaternion.identity); // Instantiate a new card from the spawn point
         newCard.transform.SetParent(transform);
+
         cardsInHand.Add(newCard); // Add the new card to the list
+
+        // Adjust the position of all cards based on the total number of cards
+        float cardWidth = newCard.GetComponent<RectTransform>().rect.width;
+        float totalWidth = cardWidth * cardsInHand.Count;
+        for (int i = 0; i < cardsInHand.Count; i++)
+        {
+            StartCoroutine(MoveCard(cardsInHand[i], new Vector3((i * cardWidth) - totalWidth / 2 + cardWidth / 2, 0, 0)));
+        }
 
         Debug.Log("New card added to hand. Total cards in hand: " + cardsInHand.Count);
     }
 
-    // Method to remove a card from the hand
+    private IEnumerator MoveCard(GameObject card, Vector3 targetPosition)
+    {
+        float duration = 1.0f; // Duration of the animation in seconds
+        float elapsed = 0.0f;
+        Vector3 startPosition = card.transform.localPosition;
+
+        while (elapsed < duration)
+        {
+            elapsed += Time.deltaTime;
+            float t = elapsed / duration;
+            t = Utility.Easing.Functions.GetEaseValue(EasingType.SineInOut, t); // Use the easing function
+            card.transform.localPosition = Vector3.Lerp(startPosition, targetPosition, t);
+            yield return null;
+        }
+
+        card.transform.localPosition = targetPosition; // Ensure the card ends up at the exact target position
+    }
+
     public void RemoveCard(GameObject card)
     {
-        // if (cardsInHand.Contains(card)) // Check if the card is in the hand
-        // {
-        //     cardsInHand.Remove(card); // Remove the card from the list
-        //     Destroy(card); // Destroy the card
+        if (cardsInHand.Contains(card)) // Check if the card is in the hand
+        {
+            cardsInHand.Remove(card); // Remove the card from the list
+            Destroy(card); // Destroy the card
 
-        //     Debug.Log("Card removed from hand. Total cards in hand: " + cardsInHand.Count);
-        // }
-
-        //for list testing purposes
+            Debug.Log("Card removed from hand. Total cards in hand: " + cardsInHand.Count);
+        }
+    }
+    public void RemoveCardBtn(GameObject card)
+    {
         if (cardsInHand.Count > 0) // Check if there are any cards in the hand
         {
             GameObject oldestCard = cardsInHand[0]; // Get the oldest card
