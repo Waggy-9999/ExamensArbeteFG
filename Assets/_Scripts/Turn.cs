6c52e1b14c6a6b80a439a32ca5b9d01dde99b44c
Viktor Palmén <viktor.palmen23@gmail.com>
2023-12-18 18:14:17 +0100

    Added Turn
    Now has a script thas manages Mana, if mana is 0, resets hand

--- Assets/_Scripts/Turn.cs
+++ Assets/_Scripts/Turn.cs
@@ -4,13 +4,20 @@ using UnityEngine;
 
 public class Turn : MonoBehaviour
 {
-    private int manaCount = 3; // Initialize manaCount to 3
+    public Hand hand; // Reference to the Hand script
+    public int manaCount = 3; // Initialize manaCount to 3
+
     public void PlayCard()
     {
         if (manaCount > 0) // Check if there is enough mana to play a card
         {
             manaCount--; // Decrease manaCount by 1
             Debug.Log("Card played. Remaining mana: " + manaCount);
+
+            if (manaCount == 0) // Check if manaCount is 0 after a card is played
+            {
+                hand.ResetHand(); // Toss all cards, reset manaCount to 3, and add multiple new cards
+            }
         }
         else
         {
